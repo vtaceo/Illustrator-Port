f20f28955da2b12524b6293a6ff0f3450cfe6645
vee <vnihil@protonmail.com>
2024-02-22 14:43:26 -0600

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    colors:{
+    'backg': "#f9bf2a",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
